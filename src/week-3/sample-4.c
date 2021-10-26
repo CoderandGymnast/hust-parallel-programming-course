f12891fb144de05ec2a3904dd4e8c9d9071e7154
Sonic <HungICT2407@gmail.com>
2021-10-26 14:39:28 +0700

    [MATRIX MULTIPLICATION]: parallel

--- src/week-3/sample-4.c
+++ src/week-3/sample-4.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
 int main()
 {
 	int a[10][10], b[10][10], mul[10][10], r, c, i, j, k;
@@ -25,19 +28,54 @@ int main()
 		}
 	}
 
-	printf("multiply of the matrix=\n");
-	for (i = 0; i < r; i++)
+	int numCols = c;
+	int NUM_THREADS = omp_get_num_threads();
+	int numColsPerThread = floor(numCols / NUM_THREADS);
+	int numColsForLastThread = numCols - (NUM_THREADS - 1) * numColsPerThread;
+
+	int lastThreadID = NUM_THREADS - 1;
+	int numNonLastThread = NUM_THREADS - 1;
+
+#pragma omp parallel
 	{
-		for (j = 0; j < c; j++)
+
+		int threadID = omp_get_thread_num();
+		if (threadID != lastThreadID)
 		{
-			mul[i][j] = 0;
-			for (k = 0; k < c; k++)
+			for (int i = 0; i < r; i++)
 			{
-				mul[i][j] += a[i][k] * b[k][j];
+				int nc = (threadID + 1) * numColsPerThread;
+				for (int j = threadID * numColsPerThread; j < nc; j++)
+				{
+
+					mul[i][j] = 0;
+					for (k = 0; k < nc; k++)
+					{
+						mul[i][j] += a[i][k] * b[k][j];
+					}
+				}
+			}
+		}
+		else
+		{ // last thread.
+			for (int i = 0; i < r; i++)
+			{
+				int nc = numCols;
+				for (int j = threadID * numColsPerThread; j < numCols; j++)
+				{
+
+					mul[i][j] = 0;
+					for (k = 0; k < nc; k++)
+					{
+						mul[i][j] += a[i][k] * b[k][j];
+					}
+				}
 			}
 		}
 	}
-	
+
+	printf("multiply of the matrix=\n");
+
 	for (i = 0; i < r; i++)
 	{
 		for (j = 0; j < c; j++)
